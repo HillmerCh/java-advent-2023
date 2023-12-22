1686d802607577ab2a29fa765c3cad72dbef89f0
HillmerCh <hilac@msn.com>
2023-12-22 21:59:54 +0000

    first commit

---
+++ src/main/java/com/example/advent2023demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.advent2023demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/advent2023demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advent2023demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
